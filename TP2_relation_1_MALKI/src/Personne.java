e604c2825b287e76521ac08bf7e2b118ff59aad7
saidm <saidm@LAPTOP-0U4R9152>
2021-11-11 19:59:48 +0100

    Plusieur erreur, revoir la methode du constructeur Probleme au niveau des classes essayer de resoudre ce probleme question 10 à refaire , pas assez compris reprendre cette exercice et comprendre les erreurs de classes

---
+++ TP2_relation_1_MALKI/src/Personne.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author saidm
+ */
+public class Personne {
+   String nom ;
+   String prenom;
+   
+   int nbVoitures ;
+   Voiture [] liste_voitures ;
+
+   public String Personne( String, String){
+    liste_voitures = new Voiture [3] ;
+    nbVoitures = 0
+   } 
+   
+   @Override
+public String toString () {
+ 
+}
+}
---
+++ TP2_relation_1_MALKI/src/Voiture.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author saidm
+ */
+public class Voiture {
+  String Modele;
+  String Marque ;
+  int PuissanceCV ;
+  String Proprietaire ;
+  Personne Proprietaire ;
+    public String Voiture(String,String,int){
+    Proprietaire= "NULL" ;
+    } 
+
+@Override
+public String toString () {
+ 
+}
+  
+
+}
+
+
+
