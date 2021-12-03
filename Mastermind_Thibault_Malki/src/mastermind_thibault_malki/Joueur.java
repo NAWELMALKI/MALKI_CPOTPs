6cc8a713eec89d18eb82c45958120303af6986c6
saidm <saidm@LAPTOP-0U4R9152>
2021-12-03 17:02:04 +0100

    Creation du MasterMind
    Creation de la classe Pions et Plateau 
    classe Pions finie
    classe Joueur finie

---
+++ Mastermind_Thibault_Malki/src/mastermind_thibault_malki/Joueur.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mastermind_thibault_malki;
+
+/**
+ *
+ * @author saidm
+ */
+public class Joueur {
+    String Nom ;
+
+    
+    Joueur(String nomjoueur){
+        Nom = nomjoueur ;
+    }
+    
+}
